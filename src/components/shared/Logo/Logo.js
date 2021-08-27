4bdc22c8e32b6635a3c465d6b7a8323e3af1a4eb
AlonBH <41362078+AlonBH@users.noreply.github.com>
2021-08-27 13:55:16 +0300

    feat(logo): created generic logo component

---
+++ src/components/shared/Logo/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100%',
+    filter: 'grayscale(1) invert(1)',
+    '& img': {
+      height: '100%'
+    }
+  },
+}));
+
+const Logo = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <img src={props.src} />
+    </div>
+  );
+};
+
+export default Logo;
