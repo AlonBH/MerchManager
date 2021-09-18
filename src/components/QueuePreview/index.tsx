5c00eda7dc7dc7251833c91b12450c60d00f264f
AlonBH <41362078+AlonBH@users.noreply.github.com>
2021-09-18 15:54:16 +0300

    ref(queue-preview): code review changes

--- src/components/QueuePreview/index.tsx
+++ src/components/QueuePreview/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core';
 import { Link } from 'react-router-dom';
+
+import useStyles from './styles';
 
 type Props = {
   name: string;
@@ -13,30 +16,25 @@ type Props = {
   link: string;
 };
 
-const useStyles = makeStyles({
-  root: {
-    
-  },
-  media: {
-    height: 140,
-  },
-});
-
 const QueuePreview = (props: Props) => {
   const classes = useStyles();
 
   return (
     <Link to={props.link}>
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia className={classes.media} image={props.imgSrc} title={props.name} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.name}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={props.imgSrc}
+            title={props.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Link>
   );
 };
