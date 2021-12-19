8c35b9692f57a7971a64706b3ddb8e2d455a64f9
Alon Ben Haim <alon.b.h.672@gmail.com>
2021-12-19 21:03:28 +0200

    style(request-details): moved style to separate file

---
+++ src/components/RequestDetails/styles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  detailsHeader: { 
+    height: '100px' 
+  },
+}));
+
+export default useStyles;
